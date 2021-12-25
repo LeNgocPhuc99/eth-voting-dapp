d5794d0d63136be2d3171bd9f64ea5bf79694892
LeNgocPhuc99 <lphuc5461@gmail.com>
2021-12-26 00:22:01 +0700

    connect to metamask

--- src/js/App.js
+++ src/js/App.js
@@ -1,9 +1,33 @@
+import { useState, useEffect } from "react";
 import Web3 from "web3";
 
 function App() {
+  const [account, setAccount] = useState();
+
+  useEffect(() => {
+    const ethEnable = async () => {
+      loadBlockchainData();
+    };
+    ethEnable();
+  }, []);
+
+  const loadBlockchainData = async () => {
+    if (window.ethereum) {
+      await window.ethereum.request({ method: "eth_requestAccounts" });
+      window.web3 = new Web3(window.ethereum);
+      // connect to metamask
+      let web3 = window.web3;
+      const account = await web3.eth.getAccounts();
+      setAccount(account);
+    } else if (!window.web3) {
+      window.alert("Metamask is not detected");
+    }
+  };
+
   return (
     <div>
       <p>Ethereum Voting Dapp</p>
+      <p>Your account: {account}</p>
     </div>
   );
 }
